dcb4a134c40529be5285bdf19728993a1f4ef0e4
root <you@example.com>
2013-03-03 15:05:48 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testlush::Application.config.secret_token = '59e1a3b7e8d89538bfb120a9b63cff83cb951a15213b38306946f1b9952f8ca72ab39e17755cd2ffd85e282536ddeffb708744ef2530fb2040d0af242aadcd2b'
